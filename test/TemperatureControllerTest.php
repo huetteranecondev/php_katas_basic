c7d6deab51cca823d75f49e9cc314bc005590c04
huetteranecondev <32643615+huetteranecondev@users.noreply.github.com>
2018-03-01 14:24:04 +0100

    skeleton

---
+++ test/TemperatureControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Afh\RoomClimate\Test;
+
+use Afh\RoomClimate\TemperatureController;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * TemperatureController test case.
+ */
+class TemperatureControllerTest extends TestCase
+{    
+
+}
+
