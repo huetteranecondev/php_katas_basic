a51103d72af84ba0258d8f81a11464ce74f4d02f
Andreas Hütter <afh.swe@gmail.comm>
2018-04-10 12:31:53 +0200

    initial setup

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -1,47 +1,11 @@
 <?php
 
-namespace Katas\LeapYear;
+namespace Katas\StringCalculator;
 
-class LeapYearChecker
+class StringCalculator
 {
 
     public function __construct()
     {}
-    
-    public function check(int $year): bool
-    {
-        if (!$this->isDivisibleByFour($year))
-        {
-            return false;
-        }
-
-        if (!$this->isDivisibleByHundred($year))
-        {
-            return true;
-        }
-        
-        if (!$this->isDivisibleByFourHundred($year))
-        {
-            return false;
-        }
-        
-        return true;
-        
-    }
-    
-    private function isDivisibleByFour(int $year): bool
-    {
-        return ($year % 4 == 0);
-    }
-    
-    private function isDivisibleByHundred(int $year): bool
-    {
-        return ($year % 100 == 0);
-    }
-    
-    private function isDivisibleByFourHundred(int $year): bool
-    {
-        return ($year % 400 == 0);
-    }
 }
 
